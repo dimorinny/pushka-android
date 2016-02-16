aa210908d5e0d7584b9742d85afa27fecf7b0b53
Dimorinny <didika914@gmail.com>
2016-02-16 21:54:10 +0300

    login screen

--- app/src/main/java/ru/nbsp/pushka/ui/login/LoginActivity.kt
+++ app/src/main/java/ru/nbsp/pushka/ui/login/LoginActivity.kt
@@ -1,6 +1,9 @@
 package ru.nbsp.pushka.ui.login
 
+import android.graphics.Typeface
 import android.os.Bundle
+import android.widget.TextView
+import com.dimorinny.vscale.dependency.bindView
 import ru.nbsp.pushka.BaseApplication
 import ru.nbsp.pushka.R
 import ru.nbsp.pushka.mvp.PresentedActivity
@@ -16,6 +19,8 @@ class LoginActivity : PresentedActivity<LoginPresenter>(), LoginView {
     @Inject
     lateinit var presenter: LoginPresenter
 
+    val textLogo: TextView by bindView(R.id.text_logo)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
@@ -23,5 +28,11 @@ class LoginActivity : PresentedActivity<LoginPresenter>(), LoginView {
         BaseApplication.graph.inject(this)
         presenter.view = this
         initPresenter(presenter)
+
+        initFont()
+    }
+
+    private fun initFont() {
+        textLogo.typeface = Typeface.createFromAsset(assets, "fonts/Bebas.ttf");
     }
 }
