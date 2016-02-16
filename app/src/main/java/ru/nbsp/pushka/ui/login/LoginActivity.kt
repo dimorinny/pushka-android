c8fe6f12e487b5118932834c0a5412ea0c815075
Dimorinny <didika914@gmail.com>
2016-02-16 22:20:23 +0300

    fix duration

--- app/src/main/java/ru/nbsp/pushka/ui/login/LoginActivity.kt
+++ app/src/main/java/ru/nbsp/pushka/ui/login/LoginActivity.kt
@@ -2,8 +2,11 @@ package ru.nbsp.pushka.ui.login
 
 import android.graphics.Typeface
 import android.os.Bundle
+import android.view.animation.AccelerateDecelerateInterpolator
 import android.widget.TextView
 import com.dimorinny.vscale.dependency.bindView
+import com.flaviofaria.kenburnsview.KenBurnsView
+import com.flaviofaria.kenburnsview.RandomTransitionGenerator
 import ru.nbsp.pushka.BaseApplication
 import ru.nbsp.pushka.R
 import ru.nbsp.pushka.mvp.PresentedActivity
@@ -16,10 +19,16 @@ import javax.inject.Inject
  */
 class LoginActivity : PresentedActivity<LoginPresenter>(), LoginView {
 
+    companion object {
+        private const val KEN_BURNS_DURATION = 20000L;
+        private const val FONT_PATH = "fonts/Bebas.ttf"
+    }
+
     @Inject
     lateinit var presenter: LoginPresenter
 
     val textLogo: TextView by bindView(R.id.text_logo)
+    val backgroundImage: KenBurnsView by bindView(R.id.login_background_image)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -30,9 +39,16 @@ class LoginActivity : PresentedActivity<LoginPresenter>(), LoginView {
         initPresenter(presenter)
 
         initFont()
+        initKenBurns()
+    }
+
+    private fun initKenBurns() {
+        backgroundImage.setTransitionGenerator(
+                RandomTransitionGenerator(KEN_BURNS_DURATION,
+                        AccelerateDecelerateInterpolator()))
     }
 
     private fun initFont() {
-        textLogo.typeface = Typeface.createFromAsset(assets, "fonts/Bebas.ttf");
+        textLogo.typeface = Typeface.createFromAsset(assets, FONT_PATH)
     }
 }
