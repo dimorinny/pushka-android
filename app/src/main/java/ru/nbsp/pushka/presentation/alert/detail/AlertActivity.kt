fd65b25a86937e8d060dab48ca8e65d539893df4
Dimorinny <didika914@gmail.com>
2016-03-17 06:15:10 +0300

    fix ugly alert activity (not ready)

--- app/src/main/java/ru/nbsp/pushka/presentation/alert/detail/AlertActivity.kt
+++ app/src/main/java/ru/nbsp/pushka/presentation/alert/detail/AlertActivity.kt
@@ -1,16 +1,27 @@
 package ru.nbsp.pushka.presentation.alert.detail
 
+import android.os.Build
 import android.os.Bundle
-import android.support.design.widget.CollapsingToolbarLayout
 import android.support.v7.app.ActionBar
+import android.support.v7.widget.CardView
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
 import android.support.v7.widget.Toolbar
 import android.view.MenuItem
+import android.view.View
 import android.webkit.WebView
 import android.webkit.WebViewClient
+import android.widget.Toast
+import com.gordonwong.materialsheetfab.DimOverlayFrameLayout
+import com.gordonwong.materialsheetfab.MaterialSheetFab
+import com.gordonwong.materialsheetfab.MaterialSheetFabEventListener
 import ru.nbsp.pushka.BaseApplication
 import ru.nbsp.pushka.R
 import ru.nbsp.pushka.presentation.PresentedActivity
+import ru.nbsp.pushka.presentation.alert.detail.adapter.AlertActionsAdapter
+import ru.nbsp.pushka.presentation.core.adapter.OnItemClickListener
 import ru.nbsp.pushka.presentation.core.model.alert.PresentationAlert
+import ru.nbsp.pushka.presentation.core.widget.AnimatedFloatingActionButton
 import ru.nbsp.pushka.util.bindView
 import javax.inject.Inject
 
@@ -25,11 +36,16 @@ class AlertActivity : PresentedActivity<AlertPresenter>(), AlertView {
 
     val contentWebView: WebView by bindView(R.id.alert_web_view)
     val toolbar: Toolbar by bindView(R.id.toolbar)
-    val collapsingToolbar: CollapsingToolbarLayout by bindView(R.id.collapsing_toolbar)
+    val floatingActionButton: AnimatedFloatingActionButton by bindView(R.id.alert_fab)
+    val overlay: DimOverlayFrameLayout by bindView(R.id.alert_overlay)
+    val sheetLayout: CardView by bindView(R.id.alert_fab_sheet)
+    val recyclerView: RecyclerView by bindView(R.id.alert_action_recycler_view)
 
     @Inject
     lateinit var presenter: AlertPresenter
     lateinit var alert: PresentationAlert
+    lateinit var alertActionsAdapter: AlertActionsAdapter
+    lateinit var sheetFab: MaterialSheetFab<AnimatedFloatingActionButton>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -52,6 +68,42 @@ class AlertActivity : PresentedActivity<AlertPresenter>(), AlertView {
     private fun initViews() {
         contentWebView.setWebViewClient(WebViewClient())
         contentWebView.settings.javaScriptEnabled = true
+
+        sheetFab = MaterialSheetFab(floatingActionButton, sheetLayout, overlay, resources.getColor(R.color.white), resources.getColor(R.color.white))
+        sheetFab.setEventListener(object : MaterialSheetFabEventListener() {
+
+            var statusBarColor: Int? = null
+
+            override fun onShowSheet() {
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                    statusBarColor = window.statusBarColor
+                    window.statusBarColor = resources.getColor(R.color.colorPrimaryDark2)
+                }
+            }
+
+            override fun onHideSheet() {
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                    window.statusBarColor = statusBarColor!!
+                }
+            }
+        })
+
+        initRecyclerView()
+    }
+
+    private fun initRecyclerView() {
+        recyclerView.layoutManager = LinearLayoutManager(this)
+
+        alertActionsAdapter = AlertActionsAdapter()
+        recyclerView.adapter = alertActionsAdapter
+
+        alertActionsAdapter.itemClickListener = object : OnItemClickListener {
+            override fun onItemClicked(index: Int, view: View) {
+                Toast.makeText(this@AlertActivity, "qwe", Toast.LENGTH_LONG).show()
+            }
+        }
+
+        alertActionsAdapter.actions = alert.actions
     }
 
     override fun initPresenter(presenter: AlertPresenter) {
@@ -73,11 +125,19 @@ class AlertActivity : PresentedActivity<AlertPresenter>(), AlertView {
         return super.onOptionsItemSelected(item)
     }
 
+    override fun onBackPressed() {
+        if (sheetFab.isSheetVisible) {
+            sheetFab.hideSheet();
+        } else {
+            super.onBackPressed();
+        }
+    }
+
     private fun initToolbar() {
         setSupportActionBar(toolbar)
         if (supportActionBar != null) {
             (supportActionBar as ActionBar).setDisplayHomeAsUpEnabled(true);
         }
-        collapsingToolbar.title = alert.title
+        supportActionBar?.title = alert.title
     }
 }
---
+++ app/src/main/java/ru/nbsp/pushka/presentation/alert/detail/adapter/AlertActionsAdapter.kt
@@ -0,0 +1,53 @@
+package ru.nbsp.pushka.presentation.alert.detail.adapter
+
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import ru.nbsp.pushka.R
+import ru.nbsp.pushka.presentation.core.adapter.OnItemClickListener
+import ru.nbsp.pushka.presentation.core.model.alert.PresentationAction
+import ru.nbsp.pushka.util.bindView
+import java.util.*
+
+/**
+ * Created by Dimorinny on 17.03.16.
+ */
+class AlertActionsAdapter : RecyclerView.Adapter<AlertActionsAdapter.ViewHolder>() {
+
+    var itemClickListener: OnItemClickListener? = null
+
+    var actions: List<PresentationAction> = ArrayList()
+        set(s : List<PresentationAction>) {
+            field = s
+            notifyDataSetChanged()
+        }
+
+
+    open inner class ViewHolder(val holderView: View) : RecyclerView.ViewHolder(holderView) {
+        val actionContainer: ViewGroup by bindView(R.id.item_alert_action_container)
+        val actionTitle: TextView by bindView(R.id.item_alert_action_title)
+
+        init {
+            if (itemClickListener != null) {
+                actionContainer.setOnClickListener {
+                    itemClickListener?.onItemClicked(adapterPosition, actionContainer)
+                }
+            }
+        }
+    }
+
+    override fun onBindViewHolder(holder: AlertActionsAdapter.ViewHolder, position: Int) {
+        val action = actions[position]
+        holder.actionTitle.text = "Открыть урл"
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup?, viewType: Int): AlertActionsAdapter.ViewHolder? {
+        return ViewHolder(LayoutInflater.from(parent?.context).inflate(R.layout.item_alert_action, parent, false))
+    }
+
+    override fun getItemCount(): Int {
+        return actions.size
+    }
+}
